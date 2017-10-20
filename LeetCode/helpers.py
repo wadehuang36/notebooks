bca73a7c59aaa7ba9c7ec6df5c9b5abab1e66b77
Wade <wadehuang36@gmail.com>
2017-10-19 22:27:58 -0400

    rename

--- LeetCode/helpers.py
+++ LeetCode/helpers.py
@@ -1,6 +1,10 @@
+import math
 from unittest import TestCase
 
 tc = TestCase()
 
 def eq(expect, actual):
     tc.assertEqual(expect, actual)
+    
+def log(string, *params):
+    print(string.format(*params))
